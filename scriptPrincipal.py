fbf2edb9031a2b14c752a5290979cf495af82a5f
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-10-16 11:29:23 +0200

    Update scriptPrincipal.py

--- scriptPrincipal.py
+++ scriptPrincipal.py
@@ -1,9 +1,10 @@
 import csv
-from character import *
+import character 
 
 
 characters_list = []
 
-c1 = Character.Character("Jean","Paul","18","astronaute","100")
+c1 = character.Character("Jean","Paul",18,"astronaute",100)
 
 characters_list.append(c1)
+
