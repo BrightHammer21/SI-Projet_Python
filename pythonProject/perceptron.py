8d4620f771bffb7a8c795c180e04e3939ea04528
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-11-12 09:41:20 +0100

    dfghjk

--- pythonProject/perceptron.py
+++ pythonProject/perceptron.py
@@ -1,19 +1,25 @@
 class perceptron:
 
-    nbIpputs=0
-    nbEpochs=0
-    learningRate=0
-    biais=1
-
-
-    def __init__(self, nbIpputs, nbEpochs, learningRate):
+    def __init__(self, nbIpputs, nbEpochs, learningRate, listeInput, listeResult):
         self.nbIpputs=nbIpputs
         self.nbEpochs=nbEpochs
         self.learningRate=learningRate
+        self.biais = 1
+
+        #POIDS
+        self.w0 = 0
+        self.w1 = 0
+        self.w2 = 0
+
+        #ENTREES ET RESULT
+        self.listeInputs = listeInput
+        self.listeResult = listeResult
+
+
 
     def predict(self, input1, input2):
         res = 0
-
+        T = input1 * self.w1 +input2 * self.w2
         if T<=0:
             res=0
         else:
