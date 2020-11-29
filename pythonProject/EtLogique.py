d063250cf8ede9084e3a2509870cabe5979ffef3
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-11-29 13:46:09 +0100

    Update EtLogique.py

--- pythonProject/EtLogique.py
+++ pythonProject/EtLogique.py
@@ -9,25 +9,27 @@ class EtLogique:
 
     def calcul(self):
         valErreurTotal = 0
-        self.listeValeurErreur =np.zeros((10,10))
+        self.listeValeurErreur =np.zeros((20,20))
 
-        for w1 in range(-5, 5):
-            for w2 in range(-5, 5):
+        for w1 in range(-10, 10,1):
+            for w2 in range(-10, 10,1):
                 for i in range(0, 4):
                     T = self.listeInput[i].__getitem__(0) * w1 + self.listeInput[i].__getitem__(1) * w2
 
-                    if T <= 0:
-                        y = 0
-                    else:
+                    if T > 0:
                         y = 1
+                    else:
+                        y = 0
 
                     erreur = 0.5 * (y - self.listeResult[i])**2
                     valErreurTotal = valErreurTotal + erreur
                     self.listeValeurErreur[w1][w2] = erreur
 
+                #self.listeValeurErreur[w1][w2] = np.sum(0.5*((((node[0]*w1)+(node[1]*w2))>0)-self.listeResult[node[0]*2+node[1]])*2 for node in self.listeInput)
 
 
     def afficherListe(self):
+
         for e in self.listeValeurErreur:
            print(str(e) + " , ")
 
