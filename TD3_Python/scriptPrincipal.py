9e90dad8391aa7295664e7735caa3492e8682b82
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-12-09 17:27:06 +0100

    Update scriptPrincipal.py

--- TD3_Python/scriptPrincipal.py
+++ TD3_Python/scriptPrincipal.py
@@ -51,13 +51,14 @@ modele = Sequential()
 modele.add(Dense(10, input_dim=7, activation='softmax'))
 modele.summary()
 
-modele.compile(optimizer=learningRate, loss='binary_crossentropy')
-modele.fit(listeInput,listeOutput, epochs=100)
+modele.compile(optimizer=learningRate, loss='binary_crossentropy')# mean_square_error
+modele.fit(listeInput,listeOutput, epochs=50)
 
 
 #PREDICTION
 valeursAAvoir =  np.array([[0,1,1,0,0,0,0],[1,1,0,1,1,0,1],[1,1,1,1,0,0,1],[0,1,1,0,0,1,1],[1,0,1,1,1,1,1],[1,0,1,1,1,1,1],[1,1,1,0,0,0,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,1]])
 predictions = modele.predict(valeursAAvoir)
+print("prédiction : ")
 print([v[0] for v in predictions])
 
 #[[0,1,1,0,0,0,0],[1,1,0,1,1,0,1],[1,1,1,1,0,0,1],[0,1,1,0,0,1,1],[1,0,1,1,1,1,1],[1,0,1,1,1,1,1],[1,1,1,0,0,0,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,1]]
