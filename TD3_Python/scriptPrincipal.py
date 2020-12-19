7b8b62dd0175d63c1cf23705c54fc2206aa17383
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-12-19 14:13:57 +0100

    Update scriptPrincipal.py

--- TD3_Python/scriptPrincipal.py
+++ TD3_Python/scriptPrincipal.py
@@ -23,23 +23,33 @@ with open('donnees_segments.csv', newline='') as fichier:
             compteur2 = compteur2 + 1
         compteur=compteur+1
 
-print("Liste input : ")
+
 def afficherListeInput():
+    print("Liste input : ")
     for element in listeInput:
         print(element)
 
-print("Liste output : ")
+
+
+
 def afficherListeOuput():
+    print("Liste output avant : ")
     for element in listeOutput:
         print(element)
 
 afficherListeInput()
+
+
 afficherListeOuput()
 
 #--------------------------------------------------------------------
 #début keras
-listeOutput = keras.utils.to_categorical(listeOutput)
-listeInput = np.reshape(listeInput,(9,1,7))
+listeOutput = keras.utils.to_categorical(listeOutput,10) #ajout du 10= nb de classes à encoder qu'on avait oublié
+#listeInput = np.reshape(listeInput,(9,1,7))
+
+
+print("\n------------------------------------\nlisteOutput avec to_categorical :")
+print(listeOutput)
 
 #teachers
 #teacher = np.array([1,1,1,1,1,1,1])
@@ -48,17 +58,34 @@ learningRate = SGD(lr=0.001)
 #modele type séquentiel
 modele = Sequential()
 
-modele.add(Dense(10, input_dim=7, activation='softmax'))
+#ajout des couches
+modele.add(Dense(10, input_dim=7, activation='relu'))
+modele.add(Dense(64, input_dim=10,activation='relu'))
+modele.add(Dense(64, input_dim=64,activation='relu'))
+modele.add(Dense(64, input_dim=64,activation='relu'))
+modele.add(Dense(64, input_dim=10,activation='relu'))
+modele.add(Dense(64, input_dim=64,activation='relu'))
+modele.add(Dense(64, input_dim=64,activation='relu'))
+modele.add(Dense(10, input_dim=64, activation='softmax'))
 modele.summary()
 
-modele.compile(optimizer=learningRate, loss='binary_crossentropy')# mean_square_error
-modele.fit(listeInput,listeOutput, epochs=50)
+modele.compile(optimizer=learningRate, loss='categorical_crossentropy')# mean_squared_error #binary_crossentropy
+modele.fit(listeInput,listeOutput, epochs=8000, batch_size=8)
 
 
 #PREDICTION
-valeursAAvoir =  np.array([[0,1,1,0,0,0,0],[1,1,0,1,1,0,1],[1,1,1,1,0,0,1],[0,1,1,0,0,1,1],[1,0,1,1,1,1,1],[1,0,1,1,1,1,1],[1,1,1,0,0,0,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,1]])
+valeursAAvoir =  np.array(listeInput)
 predictions = modele.predict(valeursAAvoir)
 print("prédiction : ")
-print([v[0] for v in predictions])
+#print([v[0] for v in predictions])
 
+for row in predictions:
+    max_row = np.max(row)
+    for j in range(0,10):
+        if row[j]==max_row:
+            row[j]=1
+        else:
+            row[j]=0
+
+print(predictions)
 #[[0,1,1,0,0,0,0],[1,1,0,1,1,0,1],[1,1,1,1,0,0,1],[0,1,1,0,0,1,1],[1,0,1,1,1,1,1],[1,0,1,1,1,1,1],[1,1,1,0,0,0,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,1]]
